3cd88bdfeb6ff79242a1f0944fec9795577b3c62
otto <otto.lindfors@abo.fi>
2019-09-29 02:07:18 +0300

    Added functionality to edit auctions. Added informational messages when auction is created and when auction is not found

--- auction/urls.py
+++ auction/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
     path('create/', views.CreateAuction.as_view(), name='create'),
-    re_path(r'^edit/(\d+)/$', views.EditAuction.as_view(), name='edit'),
+    path('edit/<int:id>/', views.EditAuction.as_view(), name='edit'),
+    # ^ mathes the beginning of a string, \d matches any digit, + matches one or more
+    # re_path(r'^edit/(\d+)/$', views.EditAuction.as_view(), name='edit'),    # url -> edit/digits
     re_path(r'^bid/(\d+)/$', views.bid, name='bid'),
     re_path(r'^ban/(\d+)$', views.ban, name='ban'),
     path('resolve/', views.resolve, name='resolve')
--- auction/views.py
+++ auction/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.http import HttpResponse, request, HttpResponseRedirect
 from django.urls import reverse
@@ -25,7 +26,6 @@ def search(request):
 
 # If the view is more complex and handles multiple request types then define it using a class
 class CreateAuction(View):
-
     def get(self, request):
         form = CreateAuctionForm()  # Create a blank form
         return render(request, "createauction.html", {"form": form})
@@ -38,16 +38,44 @@ class CreateAuction(View):
             body = cdata["body"]
 
             auction = AuctionModel(title=title, body=body)  # Create an auction, not saved anywhere yet
-            auction.save()  # Save the auction to the database
+            auction.save()                                  # Save the auction to the database
 
+            messages.add_message(request, messages.INFO, "Your auction was successfully added")
             return HttpResponseRedirect(reverse("index"))   # Redirect the user after successful auction post
         else:
+            messages.add_message(request, messages.INFO, "We tried everything. Looks like the data you gave us was "
+                                                         "invalid.")  # eBay style error message
             return render(request, "createauction.html", {"form": form})    # Give a blank form to the user if the data was not valid
 
 
-
 class EditAuction(View):
-    pass
+    def get(self, request, id):
+        auctions = AuctionModel.objects.filter(id=id)   # returns an array of matches
+        # auction = get_object_or_404(AuctionModel, id=id)
+        if len(auctions) == 1:
+            auction = auctions[0]
+            # return the pre-filled form to the user for editing
+            return render(request, "editauction.html", {"user": request.user, "title": auction.title, "id": auction.id, "body": auction.body})  # add {{max_lentgh}}
+        else:
+            messages.add_message(request, messages.INFO, "Invalid auction id")
+            return HttpResponseRedirect(reverse("auction:index"))
+
+    def post(self, request, id):
+        auctions = AuctionModel.objects.filter(id=id)   # returns an array of matches
+        if len(auctions) == 1:
+            auction = auctions[0]
+        else:
+            messages.add_message(request, messages.INFO, "Invalid auction id")
+            return HttpResponseRedirect(reverse("auction:index"))
+
+        title = request.POST["title"].strip()   # get the title from the posted data
+        body = request.POST["body"].strip()     # get the body from the posted data
+        auction.title = title
+        auction.body = body
+        auction.save()  # save the updated auction
+
+        messages.add_message(request, messages.INFO, "Auction updated")
+        return HttpResponseRedirect(reverse("auction:index"))
 
 
 def bid(request, item_id):
