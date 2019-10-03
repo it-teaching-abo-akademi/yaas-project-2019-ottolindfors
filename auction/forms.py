a5f440a7ce6254f737d424f820152cdd4b49742d
otto <otto.lindfors@abo.fi>
2019-10-03 18:18:49 +0300

    Added minimum_price, deadline and status to auction creation.

--- auction/forms.py
+++ auction/forms.py
@@ -4,3 +4,8 @@ from django import forms
 class CreateAuctionForm(forms.Form):
     title = forms.CharField(max_length=255)
     description = forms.CharField(widget=forms.Textarea())
+    minimum_price = forms.FloatField(min_value=0.01)
+    deadline = forms.DateTimeField(input_formats=['%d.%m.%Y %H:%M:%S'],
+                                   widget=forms.TextInput(attrs={"placeholder": "dd.mm.yyyy HH:MM:SS"}),
+                                   help_text="Give the date as dd.mm.yyyy HH:MM:SS",
+                                   label="Deadline")
--- auction/views.py
+++ auction/views.py
@@ -9,27 +9,16 @@ from .models import AuctionModel
 from .forms import CreateAuctionForm
 
 
-# If the view is simple and handling only one request type then it is okay to define it using a function
 def index(request):
     auctions = AuctionModel.objects.filter(status='Active').order_by('deadline')  # nearest deadline first
     print(auctions)  # debugging
     return render(request, "index.html", {"auctions": auctions})
 
 
-'''
-def index(request):
-    print(request.headers)
-    print("creating response...")
-    html = "<html><body>Hello! <br> <p> This was your request: %s %s <p> sent from the following browser: %s </body></html>" % (request.method, request.path, request.headers['User-Agent'])
-    return HttpResponse(html)
-'''
-
-
 def search(request):
     pass
 
 
-# If the view is more complex and handles multiple request types then define it using a class
 @method_decorator(login_required, name='dispatch')
 class CreateAuction(View):
     def get(self, request):
@@ -40,18 +29,18 @@ class CreateAuction(View):
         form = CreateAuctionForm(request.POST)   # Create a form with the data the user has POSTed to us
         if form.is_valid():
             cdata = form.cleaned_data
-            title = cdata["title"]
-            description = cdata["description"]
-
-            auction = AuctionModel(title=title, description=description)  # Create an auction, not saved anywhere yet
-            auction.save()                                  # Save the auction to the database
+            new_auction = AuctionModel(title=cdata["title"],
+                                   description=cdata["description"],
+                                   minimum_price=cdata["minimum_price"],
+                                   deadline=cdata["deadline"])  # Create an auction, not saved anywhere yet
+            new_auction.save()  # Save the auction to the database
 
             messages.add_message(request, messages.INFO, "Your auction was successfully added")
             return HttpResponseRedirect(reverse("index"))   # Redirect the user after successful auction post
         else:
             messages.add_message(request, messages.INFO, "We tried everything. Looks like the data you gave us was "
-                                                         "invalid.")  # eBay style error message
-            return render(request, "createauction.html", {"form": form})    # Give a blank form to the user if the data was not valid
+                                                         "invalid.")
+            return render(request, "createauction.html", {"form": form})  # Give a blank form to the user if the data was not valid
 
 
 @method_decorator(login_required, name='dispatch')
