2de65aa95f0ed59f1459e91f55c3a4b5dd7b260c
otto <otto.lindfors@abo.fi>
2019-10-03 12:50:57 +0300

    Added auction minimum_price, deadline and status

--- auction/models.py
+++ auction/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 
@@ -5,5 +7,13 @@ from django.db import models
 class AuctionModel(models.Model):
     title = models.CharField(max_length=255)    # Title of the auction
     description = models.TextField()  # Body or description of the auction
+    minimum_price = models.FloatField(default=0.0)
     timestamp = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(default=datetime.now())  # default=datetime.now()
+    status = models.CharField(max_length=1024, default="Active")
+
+
+# Override default function __str__(self) to print a string presentation of the object instead of memory address
+def __str__(self):
+    return self.title
 
