8a27e712011b8874d29f45d0920e625b5fe1e69e
otto <otto.lindfors@abo.fi>
2019-10-06 13:19:07 +0300

    Added seller to an auction. Created Bid model

--- auction/models.py
+++ auction/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Auction model (table)
@@ -11,9 +12,32 @@ class AuctionModel(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     deadline_date = models.DateTimeField(default=datetime.now())  # default=datetime.now()
     status = models.CharField(max_length=1024, default="Active")
+    seller = models.ForeignKey(User, on_delete=models.SET_NULL)
+    # seller = models.ForeignKey(SellerUserMap, on_delete=models.PROTECT)
+    '''
+    By default the AuctionModel instance (an auction) would be deleted if the User (seller) is deleted. To prevent this
+    the on_delete is set to null by on_delete=models.SET_NULL.
+
+    With GDPR in mind the foreign key should not be the User. Instead it could be a integer/hash in a table that maps
+    integers to users. If a user is deleted admins can still see the items sold by the same user but the username will 
+    not be avalible anymore.
+
+    Example: 
+    AuctionModel(seller_id, ...)
+    SellerUserMap(seller_id, username)
+    User(username, ...)
+
+    Before deletion of a user the auction is shown with the seller as 'username'.
+    After deletion of a user the auction is shown with the seller as 'username'. Only admins can see the auctions.
+    '''
 
 
 # Override default function __str__(self) to print a string presentation of the object instead of memory address
 def __str__(self):
     return self.title
 
+class Bid(models.Model):
+    # An auction should not be deleted but on_delete=SET_NULL just in case
+    auction = models.ForeignKey(AuctionModel, on_delete=models.SET_NULL)
+    bidder = models.ForeignKey(User, on_delete=models.SET_NULL)
+    price = models.FloatField(default=0.0)
