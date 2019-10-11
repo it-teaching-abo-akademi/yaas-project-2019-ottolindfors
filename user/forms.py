a4b6a850c114a211bec0389e287f978ec90961f0
otto <otto.lindfors@abo.fi>
2019-10-12 01:23:42 +0300

    Added CustomUserCreationForm.save() method. Solved issues with users not being able to sign up and sign in

--- user/forms.py
+++ user/forms.py
@@ -11,7 +11,15 @@ class CustomUserCreationForm(ModelForm):
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password']
+        fields = ('username', 'email', 'password')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
 
 
 class EditUserForm(ModelForm):
