52d2bc555507bccf746b3b9645d26f3f21cfc9bd
otto <otto.lindfors@abo.fi>
2019-10-13 14:47:13 +0300

    Some ideas in comments

--- auction/services.py
+++ auction/services.py
@@ -1,8 +1,24 @@
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from auction.models import AuctionModel
+from auction.serializers import AuctionSerializer
 
 
 class BrowseAuctionApi(APIView):
     pass
+
+# @renderer_classes([JSONRenderer])   # Only return JSON format, not XML or other
+# class BrowseAuctionApi(APIView):
+#     def get(self, request):
+#         auctions = AuctionModel.objects.all()
+#         auctions_serialized = AuctionSerializer(auctions, many=True)
+#         return Response(auctions_serialized.data)
+#
+#     def post(self, request):
+#         pass
 
 
 class SearchAuctionApi(APIView):
